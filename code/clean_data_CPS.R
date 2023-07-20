c7f4fde5017a991c75fe1fe0c8010638ecfa71f4
Benjamin-Samuel Schlüter <benjamin-samuel.schluter@uclouvain.be>
2023-07-20 17:06:35 -0400

    try modeling p0 with generalized logistic curve

--- code/clean_data_CPS.R
+++ code/clean_data_CPS.R
@@ -12,7 +12,7 @@ source(here("code", "secrets.R"))
 ## Load data -------------------------------------------------------------------
 
 ## Adapt code below to work on frever
-## Careful about age of mother
+## Careful about age of mother: not the same boundaries over the years
 # https://cps.ipums.org/cps-action/variables/FREVER#universe_section
 
 ## read_csv allows to open compressed .gz 
@@ -41,11 +41,7 @@ names(data) <- tolower(names(data))
 
 ## Number of minor per person --------------------------------------------------
 
-## Consider individuals aged >= 10 yo (to be filtered later)
-
-## What about weigths?
-
-## Minimum nber of col to optimize code
+## Tidy
 df <- data |> 
     ## Only keep year where frever (nber of children
     ## ever born) was recorded
@@ -53,26 +49,65 @@ df <- data |>
     dplyr::select(
         year, month, serial,pernum, ## identification
         momloc, momloc2, poploc, poploc2, frever,## parenthood
-        wtfinl, ## weights
+        wtfinl, ## weights,
+        statefip, ## states
         age, sex, race, hispan) |> ## demographic
-    mutate(nminor = 0)
-
-## Keep only women since 1980, 
-## not having 999 for frever
-df.p <- df |> 
+    mutate(
+        ## Create abridged race (!! NEED TO BE CHECKED !!)
+        race_eth = case_when(race == 100 & hispan == 0 ~ "NH-White",
+                             race == 200 & hispan == 0 ~ "NH-Black",
+                             !(race %in% c(100, 200)) & hispan == 0 ~ "NH-Other",
+                             hispan != 0 ~ "Hispanic")
+    ) |> 
+    ## Keep only women since 1990, 
+    ## aged 15-45 not having 999
     filter(year >= 1990,
-           sex ==2,
+           sex == 2,
+           age >= 15,
+           age < 45,
            frever != 999)
 
-## Parity per age and sex
-df.p <- df.p |> 
+## Parity per age 
+df.p <- df |> 
     mutate(
         ## Rescale weights
         w = wtfinl/1000,
         ## Create 4+ nber of minor
         nchild = ifelse(frever >= 4, 4, frever)
     ) |> 
-    group_by(age, sex, year,
+    group_by(age, sex, year, 
+             .drop = F) |> 
+    summarise(p0 = sum(w[nchild == 0])/sum(w),
+              p1 = sum(w[nchild == 1])/sum(w),
+              p2 = sum(w[nchild == 2])/sum(w),
+              p3 = sum(w[nchild == 3])/sum(w),
+              p4 = sum(w[nchild == 4])/sum(w)) 
+
+## Parity per age, and race
+df.p.race <- df |> 
+    mutate(
+        ## Rescale weights
+        w = wtfinl/1000,
+        ## Create 4+ nber of minor
+        nchild = ifelse(frever >= 4, 4, frever)
+    ) |> 
+    group_by(age, sex, year, race_eth, 
+             .drop = F) |> 
+    summarise(p0 = sum(w[nchild == 0])/sum(w),
+              p1 = sum(w[nchild == 1])/sum(w),
+              p2 = sum(w[nchild == 2])/sum(w),
+              p3 = sum(w[nchild == 3])/sum(w),
+              p4 = sum(w[nchild == 4])/sum(w)) 
+
+## Parity per age, and states
+df.p.states <- df |> 
+    mutate(
+        ## Rescale weights
+        w = wtfinl/1000,
+        ## Create 4+ nber of minor
+        nchild = ifelse(frever >= 4, 4, frever)
+    ) |> 
+    group_by(age, sex, year, statefip, 
              .drop = F) |> 
     summarise(p0 = sum(w[nchild == 0])/sum(w),
               p1 = sum(w[nchild == 1])/sum(w),
@@ -100,7 +135,7 @@ combined.df <- bind_rows(
 
 ## Visu ------------------------------------------------------------------------
 
-## CPS Fertility data 2019
+## CPS Fertility data 2020
 df.p |> 
     pivot_longer(p0:p4, names_to = "parity", values_to = "p") |> 
     mutate(sex = ifelse(sex == 2, "Female", "Male")) |> 
@@ -120,13 +155,115 @@ combined.df |>
               linewidth = 0.8) +
     theme_bw()
 ## Pros:
-# Both gender
 # Important sample size
 
-## Cons:
-# Survey is HH-based
-# As parent age, less child in HH
-# Divorce and (non) shared custody
-# Constraining child to be <18 yo
+## CPS Fertility data 2020 by race
+df.p.race |> 
+    pivot_longer(p0:p4, names_to = "parity", values_to = "p") |> 
+    mutate(sex = ifelse(sex == 2, "Female", "Male")) |> 
+    filter(year == 2020) |> 
+    ggplot(aes(x = age, y = p, 
+               group = parity,
+               col = parity)) + 
+    facet_wrap(~ race_eth) +
+    geom_line(linewidth = 1) +
+    theme_bw()
+
+## CPS Fertility data 2020 by states
+df.p.states |> 
+    pivot_longer(p0:p4, names_to = "parity", values_to = "p") |> 
+    mutate(sex = ifelse(sex == 2, "Female", "Male")) |> 
+    filter(year == 2020) |> 
+    ggplot(aes(x = age, y = p, 
+               group = parity,
+               col = parity)) + 
+    facet_wrap(~ statefip) +
+    geom_line(linewidth = 1) +
+    theme_bw()
 
 
+
+## Parametric function to model p ----------------------------------------------
+
+## Logistic 
+## Janoscheck is easy to fit and has flex inflexion point
+## Richards (gen logistic) is hard to fit but super flex
+
+## Focusing on parity 0
+logistic_fct <- function(x, x0=30, U=1, k) {
+    
+    out <- U / (1 + exp( k*(x - x0) ))
+    return(out)
+}
+
+gen_log_fct <- function(x, x0=30, L , U=1, k) {
+    
+    out <- L + ((U - L) / (1 + exp( k*(x - x0) )))
+    return(out)
+}
+
+janoscheck_fct <- function(x, x0=30, L, U=1, k) {
+    
+    out <- U - (U - L)*exp(k*x^x0)
+    return(out)
+}
+
+richards_fct <- function(x, x0=30, L, U=1, d, k) {
+    
+    out <- L + (U - L)/(1 + (d - 1)*exp(k * (x - x0) ))^(1 - d)
+    return(out)
+}
+
+df.p |> 
+    pivot_longer(p0:p4, names_to = "parity", values_to = "p") |> 
+    mutate(sex = ifelse(sex == 2, "Female", "Male")) |> 
+    filter(year == 2020,
+           parity == "p0") |> 
+    mutate(fit_p0 = logistic_fct(age, k = 0.24),
+           gen_fit_p0 = gen_log_fct(age, x0= 27, L=0.15, k = 0.24),
+           jano_fit_p0 = janoscheck_fct(age, L= 0.2, k = 0.24),
+           rich_fit_p0 = richards_fct(age, L=0.2, k=0.24, d=2)) |> 
+    ggplot(aes(x = age, y = p)) + 
+    geom_point() +
+    geom_line(aes(y = fit_p0)) +
+    geom_line(aes(y = gen_fit_p0), col = "red3") +
+    # geom_line(aes(y = rich_fit_p0), col = "pink") +
+    # geom_line(aes(y = jano_fit_p0), col = "green3") +
+    theme_bw()
+
+
+
+## Trying to use optim() -------------------------------------------------------------------------
+
+df.rss <- df.p |> 
+    pivot_longer(p0:p4, names_to = "parity", values_to = "p") |> 
+    mutate(sex = ifelse(sex == 2, "Female", "Male")) |> 
+    filter(year == 2020,
+           parity == "p0") |> 
+    ungroup() |> 
+    dplyr::select(age, p)
+
+min.RSS <- function(data, par) {
+    with(data, sum( ((par[1] + ((1-par[1])/(1+exp(par[2]*(age-par[3]))))) -  p)^2 ))
+}
+
+pars <- optim(par = c(0.15, 0.24, 27), 
+              fn = min.RSS, 
+              data = df.rss,
+              method = "L-BFGS-B",
+              lower = c(0, 0, 0),
+              upper = c(0.5, 0.5, 40))$par
+
+
+df.p |> 
+    pivot_longer(p0:p4, names_to = "parity", values_to = "p") |> 
+    mutate(sex = ifelse(sex == 2, "Female", "Male")) |> 
+    filter(year == 2020,
+           parity == "p0") |> 
+    mutate(gen_fit_p0 = gen_log_fct(age, x0= pars[3], L=pars[1], k = pars[2])) |> 
+    ggplot(aes(x = age, y = p)) + 
+    geom_point() +
+    geom_line(aes(y = gen_fit_p0), col = "red3") +
+    # geom_line(aes(y = rich_fit_p0), col = "pink") +
+    # geom_line(aes(y = jano_fit_p0), col = "green3") +
+    theme_bw()
